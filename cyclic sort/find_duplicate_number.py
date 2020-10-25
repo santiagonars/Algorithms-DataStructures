6815cba6b0adac6b1de9529fdbb074138bba34bf
Santiago Norena <san.norena@gmail.com>
2020-10-25 18:35:03 -0400

    Update find_duplicate_number.py

--- cyclic sort/find_duplicate_number.py
+++ cyclic sort/find_duplicate_number.py
@@ -14,17 +14,24 @@ Output: 3
 
 Example 3:
 Input: [2, 4, 1, 4, 4]
-Output: 4 """
-# NOTE: Time complexity => 
-# NOTE: Space complexity =>
+Output: 4
+# -------------------Similar Problems-------------------
+Problem: 
+>>> Can we solve the above problem in O(1) space and without modifying the input array?
+
+Solution: While doing the cyclic sort, we realized that the array will have a cycle due to the duplicate number 
+and that the start of the cycle will always point to the duplicate number. 
+This means that we can use the fast & the slow pointer method to find the duplicate number 
+or the start of the cycle similar to Start of LinkedList Cycle. """
+# NOTE: Time complexity => O(N)
+# NOTE: Space complexity => O(1) but modifies input array
 
 
-def find_duplicate_number(nums):
+def find_duplicate(nums):
     i = 0
-
     while i < len(nums):
-        # check if number is in the correct location
-        if nums[i] != i + 1:
+        # first check if number is in the correct location
+        if nums[i] != i + 1: # add 1 because range starts at 1
             j = nums[i] - 1
             if nums[i] != nums[j]:
                 nums[i], nums[j] = nums[j], nums[i]
@@ -32,13 +39,47 @@ def find_duplicate_number(nums):
                 return nums[i] # there is only one duplicate
         else:
             i += 1
-    return []
+    return -1
+
+# Solution in O(1) space without needing to modify the input array; the start of the cycle will always point to the duplicate number
+def find_duplicate_without_modifying(arr):
+    slow, fast = arr[0], arr[arr[0]]
+    while slow != fast: # they meet bc there's a cycle due to the duplicate
+        slow = arr[slow]
+        fast = arr[arr[fast]]
+
+    # find cycle length
+    current = arr[arr[slow]]
+    cycleLength = 1
+    while current != arr[slow]:
+        current = arr[current]
+        cycleLength += 1
+
+    return find_start(arr, cycleLength)
+
+def find_start(arr, cycleLength):
+    pointer1, pointer2 = arr[0], arr[0]
+    # move pointer2 ahead 'cycleLength' steps
+    while cycleLength > 0:
+        pointer2 = arr[pointer2]
+        cycleLength -= 1
+
+    # increment both pointers until they meet at the start of the cycle
+    while pointer1 != pointer2:
+        pointer1 = arr[pointer1]
+        pointer2 = arr[pointer2]
+
+    return pointer1
 
 
 def main():
-    print(find_duplicate_number([1, 4, 4, 3, 2])) # expected output = 4
-    print(find_duplicate_number([2, 1, 3, 3, 5, 4])) # expected output = 3
-    print(find_duplicate_number([2, 4, 1, 4, 4])) # expected output = 4
+    print(find_duplicate([1, 4, 4, 3, 2])) # expected output = 4
+    print(find_duplicate([2, 1, 3, 3, 5, 4])) # expected output = 3
+    print(find_duplicate([2, 4, 1, 4, 4])) # expected output = 4
+    print("----")
+    print(find_duplicate_without_modifying([1, 4, 4, 3, 2])) # expected output = 4
+    print(find_duplicate_without_modifying([2, 1, 3, 3, 5, 4])) # expected output = 3
+    print(find_duplicate_without_modifying([2, 4, 1, 4, 4])) # expected output = 4
 
 
 main()
