f240c22e4c343805c69cbc03894dbe9956f3fa70
Santiago Norena <san.norena@gmail.com>
2020-11-05 19:52:18 -0500

    Update binary_tree_path_sum.py

--- tree depth first search (DFS)/binary_tree_path_sum.py
+++ tree depth first search (DFS)/binary_tree_path_sum.py
@@ -1,4 +1,4 @@
-""" Binary Tree Path Sum (easy)
+""" --------Binary Tree Path Sum (easy)--------
 Problem Statement #
 >>> Given a binary tree and a number ‘S’, 
 find if the tree has a path from root-to-leaf such that the sum of all the node values of that path equals ‘S’. 
