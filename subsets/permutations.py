e16b1dd80b95c6da63d5fc5a9898741570557097
Santiago Norena <san.norena@gmail.com>
2020-11-10 20:09:44 -0500

    Added recursive solution as well

--- subsets/permutations.py
+++ subsets/permutations.py
@@ -49,8 +49,30 @@ def find_permutations(nums):
     return result
 
 
+# ALTERNATIVE: Recursive solution---------------------------------
+def generate_permutations(nums):
+    result = []
+    generate_permutations_recursive(nums, 0, [], result)
+    return result
+
+
+def generate_permutations_recursive(nums, index, currentPermutation, result):
+    if index == len(nums):
+        result.append(currentPermutation)
+    else:
+        # create a new permutation by adding the current number at every position
+        for i in range(len(currentPermutation)+1):
+            newPermutation = list(currentPermutation)
+            newPermutation.insert(i, nums[index])
+            generate_permutations_recursive(
+                nums, index + 1, newPermutation, result)
+# --------------------------------------------------------------
+
 def main():
+    # expected output = [1,3,5], [1,5,3], [3,1,5], [3,5,1], [5,1,3], [5,3,1]
     print("Here are all the permutations: " + str(find_permutations([1, 3, 5])))
+    # expected output = [1,3,5], [1,5,3], [3,1,5], [3,5,1], [5,1,3], [5,3,1]
+    print("Here are all the permutations: " + str(generate_permutations([1, 3, 5])))
 
 
 main()
