806528f2fa26b3f56698c73d47de9e40115e9876
Santiago Norena <san.norena@gmail.com>
2020-11-10 20:01:16 -0500

    Update BST_validate.py

--- archives/BST_validate.py
+++ archives/BST_validate.py
@@ -19,12 +19,20 @@ def main():
 
     print(isValidBST(tree.root))
 
+
 def isValidBST(root):
     # input format must be tree node
 
     # return Bool
     pass
 
+# NOTE: 
+# One way to do that could be to write a function that returns a recursive function 
+# that get's passed the root of the tree and checks that the if currentNode.left >= currentNode.right, then return false. 
+# This function would return true only  if calling itselt on the currentNode.left and currentNode.right are both true.  
+# At the beginning of that recursive function need too add a condition that checks if it's null, return true, 
+#       meaning it searched every node passed the leaf and they all follow the conditions of a BST
+# I think that would be O(N) time complexity since it's checking every value once
 
 if __name__ == "__main__":
     main()
