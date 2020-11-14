453ed6f3052743a9116dabe2768af7b55206aeaf
Santiago Norena <san.norena@gmail.com>
2020-11-14 18:46:29 -0500

    Update find_missing_number.py

--- bitwise XOR/find_missing_number.py
+++ bitwise XOR/find_missing_number.py
@@ -13,17 +13,17 @@ Answer: 3 """
 
 
 def find_missing_number(arr):
-    n = len(arr) + 1
-    # x1 represents XOR of all values from 1 to n
-    x1 = 1
-    for i in range(2, n+1):
+    n = len(arr) + 1 # since input array is (n - 1), we are adding 1 to be able to represent actually length of n
+    # x1 represents XOR of all values from 1 to n 
+    x1 = 1 
+    for i in range(2, n+1): # XOR from 1 to n number is =>  1 ^ 2 = XOR ^ 3 = XOR ^ 4 = XOR ^ 5 = XOR ^ 6
         print('{} ^ {}'.format(x1, i))
         x1 = x1 ^ i
         print("x1: ", x1)
     print()
-    # x2 represents XOR of all values in arr
+    # x2 represents XOR of all values in arr;
     x2 = arr[0]
-    for i in range(1, n-1):
+    for i in range(1, n-1): # the input array was n-1 so we need to subtract 1 
         print('{} ^ {}'.format(x2, i))
         x2 = x2 ^ arr[i]
         print("x2: ", x2)
@@ -55,3 +55,9 @@ def main():
     # print('Missing number NOT using XOR is: ' + str(find_missing_number_without_XOR(arr)))
 		
 main()
+
+# NOTE: Steps to find the missing number using XOR:
+		
+# 	    1) XOR all the numbers from 1 to n, let’s call it x1.
+# 		2) XOR all the numbers in the input array, let’s call it x2.
+#       3) The missing number can be found by x1 XOR x2.
