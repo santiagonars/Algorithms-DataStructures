abf32645fb2a71115bfec2406391c4a254fee7c6
Santiago Norena <san.norena@gmail.com>
2021-02-13 19:40:18 -0500

    Update permutations.py

--- Leetcode/medium/permutations.py
+++ Leetcode/medium/permutations.py
@@ -46,5 +46,23 @@ class Solution:
         return output
 
 
+def generate_permutations(nums):
+    result = []
+    generate_permutations_recursive(nums, 0, [], result)
+    return result
+
+
+def generate_permutations_recursive(nums, index, currentPermutation, result):
+    if index == len(nums):
+        result.append(currentPermutation)
+    else:
+        # create a new permutation by adding the current number at every position
+        for i in range(len(currentPermutation)+1):
+            newPermutation = list(currentPermutation)
+            newPermutation.insert(i, nums[index])
+            generate_permutations_recursive(nums, index + 1, newPermutation, result) # recursive function 
+
 solution = Solution()
 print(solution.permute([1,2,3]))
+
+print("Permutations (recursively): " + str(generate_permutations([1, 3, 5])))
