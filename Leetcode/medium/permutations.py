25e72921e5228f06658e336bcefab17979959b1c
Santiago Norena <san.norena@gmail.com>
2021-02-13 19:33:36 -0500

    Create permutations.py

---
+++ Leetcode/medium/permutations.py
@@ -0,0 +1,50 @@
+""" ----------Permutations----------
+Problem:
+>>> Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Example 2:
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+
+Example 3:
+Input: nums = [1]
+Output: [[1]]
+
+Constraints:
+    1 <= nums.length <= 6
+    -10 <= nums[i] <= 10
+    All the integers of nums are unique. """
+
+# NOTE: Use recursive solution instead that is O(N!); backtracking here is a bit slower than O(N!) but faster than O(N*N!)
+
+class Solution:
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def backtrack(first = 0):
+            # if all integers are used up
+            if first == n:  
+                output.append(nums[:])
+            for i in range(first, n):
+                # place i-th integer first 
+                # in the current permutation
+                nums[first], nums[i] = nums[i], nums[first]
+                # use next integers to complete the permutations
+                backtrack(first + 1)
+                # backtrack
+                nums[first], nums[i] = nums[i], nums[first]
+        
+        n = len(nums)
+        output = []
+        backtrack()
+        return output
+
+
+solution = Solution()
+print(solution.permute([1,2,3]))
