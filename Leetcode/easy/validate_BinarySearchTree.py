182ac33802711d8ab1a05c4ce1a144d18d9abced
Santiago Norena <san.norena@gmail.com>
2022-05-01 19:55:19 -0400

    Update validate_BinarySearchTree.py

--- Leetcode/easy/validate_BinarySearchTree.py
+++ Leetcode/easy/validate_BinarySearchTree.py
@@ -31,48 +31,88 @@ Explanation: The root node's value is 5 but its right child's value is 4. """
 # Space complexity => O(N)
 import math
 
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-# Most efficient solution
+
+""" ----- SOLUTION: ALTERNATIVE solution - works but not the best way ----- """
+# def isValidBST(root):
+
+#     if root is None or (root.left is None and root.right is None):
+#         return True
+#     # conditions that invalidate to be a BST
+#     if root.right is None and root.left.val > root.val:
+#         return False
+#     if root.left is None and root.right.val < root.val:
+#         return False
+#     if root.left.val > root.val:
+#         return False
+#     if root.right.val < root.val:
+#         return False
+#     if isValidBST(root.left) is False or isValidBST(root.right) is False:
+#         return False
+#     return True
+
+
+""" ----- SOLUTION: Simple solution (Not very efficient)----- """
+# def isValidBST(root):
+#     return checkBST(root)
+
+# def checkBST(node, lower=-math.inf, upper=math.inf):
+#     # if it's a NULL
+#     if node is None:
+#         return True
+#     # conditions that invalidate it to be a BST
+#     if node.val <= lower or node.val >= upper:
+#         return False
+#     if not checkBST(node.left, lower, node.val):
+#         return False
+#     if not checkBST(node.right, node.val, upper):
+#         return False
+#     return True
+
+
+""" ----- SOLUTION: Recursive Traversal with Valid Range ----- """
+# def isValidBST(root):
+#     def validate(node, lower=-math.inf, upper=math.inf):
+#         if node is None:
+#             return True
+        
+#         if node.val <= lower or node.val >= upper:
+#             return False
+#         return (validate(node.left, lower, node.val)) and (validate(node.right, node.val, upper))
+
+#     return validate(root)
+#     # O(N) time for recursive call for every node
+#     # O(N) space to store the calls the calls of the entire tree
+
+
+""" ----- SOLUTION: Iterative Traversal with Valid Range -----
+Uses a stack (for BFS) instead of recursition
+For this problem, recursion works better """
 def isValidBST(root):
-    return checkBST(root)
-
-def checkBST(node, lower=-math.inf, upper=math.inf):
-    # if it's a NULL
-    if node is None:
+    if not root:
         return True
-    # conditions that invalidate it to be a BST
-    if node.val <= lower or node.val >= upper:
-        return False
-    if checkBST(node.left, lower, node.val) is False:
-        return False
-    if checkBST(node.right, node.val, upper) is False:
-        return False
+    
+    stack = [(root, -math.inf, math.inf)]
+    while stack:
+        root, lower, upper = stack.pop()
+        if not root:
+            continue
+        val = root.val
+        if val <= lower or val >= upper:
+            return False
+        stack.append((root.left, lower, val))
+        stack.append((root.right, val, upper))
     return True
+    # Time: O(N) to go through every node
+    # Space: O(N) append the tree to stack
 
 
-# ALTERNATIVE solution - works but not the best way
-def isValidBST_2(root):
 
-    if root is None or (root.left is None and root.right is None):
-        return True
-    # conditions that invalidate to be a BST
-    if root.right is None and root.left.val > root.val:
-        return False
-    if root.left is None and root.right.val < root.val:
-        return False
-    if root.left.val > root.val:
-        return False
-    if root.right.val < root.val:
-        return False
-    if isValidBST_2(root.left) is False or isValidBST_2(root.right) is False:
-        return False
-    return True
 
 
 if __name__ == "__main__":
@@ -88,14 +128,3 @@ if __name__ == "__main__":
     root.left.right = TreeNode(6) 
     print("Is a valid BST: ", isValidBST(root)) # expected = False
 
-    # root = TreeNode(2) 
-    # root.left = TreeNode(1) 
-    # root.right = TreeNode(3) 
-    # print("Is a valid BST: ", isValidBST_2(root)) # expected = True
-
-    # root = TreeNode(5) 
-    # root.left = TreeNode(1) 
-    # root.right = TreeNode(4) 
-    # root.left.left = TreeNode(3)
-    # root.left.right = TreeNode(6) 
-    # print("Is a valid BST: ", isValidBST_2(root)) # expected = False
