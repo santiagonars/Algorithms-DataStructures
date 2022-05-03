319e2701c09822593cca7b411fdb77b77270dce0
Santiago Norena <san.norena@gmail.com>
2022-05-02 20:33:37 -0400

    Update binary_level_order_traversal.py

--- Leetcode/medium/binary_level_order_traversal.py
+++ Leetcode/medium/binary_level_order_traversal.py
@@ -1,4 +1,5 @@
-""" ------- Binary Tree Level Order Traversal -------
+
+""" -------  Binary Tree Level Order Traversal -------
 Given the root of a binary tree, return the level order traversal of its nodes' values. 
 (i.e., from left to right, level by level).
 
